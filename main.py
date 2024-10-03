887dfcb8316ea6109e12e2dbed29f50454038b3f
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-03 17:22:47 +0530

    added description of page

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -9,9 +11,15 @@ with col1:
 
 with col2:
     st.title("Smugcurve13")
-    content = """
+    my_desc = """
     Hi, I am Smugcurve13! I am a python programmer based out of India. I have recently learned how to make apps using python.
     I will be logging all of my current and finished projects in this portfolio website.
     More pages to come soon here. Stay Tuned!
     """
-    st.info(content)
+    st.info(my_desc)
+
+content = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+
+st.write(content)
