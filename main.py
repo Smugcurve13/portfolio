80854e3ebbf089f79b0cfc0420b848f230479a62
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-03 17:52:17 +0530

    added app titles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 
 
 st.set_page_config(layout="wide")
@@ -23,3 +23,14 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 
 st.write(content)
+
+col3,col4 = st.columns(2)
+
+df = pd.read_csv("data.csv",sep=";")
+with col3:
+    for index, row in df[:20:2].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[1:20:2].iterrows():
+        st.header(row["title"])
