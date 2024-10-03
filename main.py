d24faf51ce24b41f15ddd04ee2ce4930a60f41fb
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-03 17:12:32 +0530

    added main.py, added pfp, title text and columns

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("https://avatars.githubusercontent.com/u/48555604?v=4")
+
+with col2:
+    st.title("Smugcurve13")
+    content = """
+    Hi, I am Smugcurve13! I am a python programmer based out of India. I have recently learned how to make apps using python.
+    I will be logging all of my current and finished projects in this portfolio website.
+    More pages to come soon here. Stay Tuned!
+    """
+    st.info(content)
