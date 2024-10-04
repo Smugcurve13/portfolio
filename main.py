5d1f906dfc14aa538c8a7d6f1f7291b2736547de
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-04 11:10:03 +0530

    added app description, images and links. changed data.csv to include portfolio url

--- main.py
+++ main.py
@@ -24,13 +24,19 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 
 st.write(content)
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5, 0.5 , 1.5]) #ratio of sizes of columns
 
 df = pd.read_csv("data.csv",sep=";")
 with col3:
     for index, row in df[:20:2].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/"+row['image'])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[1:20:2].iterrows():
         st.header(row["title"])
+        st.write(row['description'])
+        st.image("images/"+row['image'])
+        st.write(f"[Source Code]({row['url']})")
